926f14cc40397d20836d674925228079534ced51
ivymso13 <37726188+ivymso13@users.noreply.github.com>
2023-12-31 22:37:05 +0900

    [Bronze II] Title: 문어, Time: 124 ms, Memory: 109108 KB -BaekjoonHub

---
+++ 백준/Bronze/21313. 문어/문어.py
@@ -0,0 +1,16 @@
+N = int(input())
+
+if N%2 == 0:
+    for i in range(N):
+        if i%2 == 0:
+            print(1, end=" ")
+        else:
+            print(2, end=" ")
+
+else:
+    for i in range(N-1):
+        if i%2 == 0:
+            print(1, end=" ")
+        else:
+            print(2, end=" ")
+    print("3")
