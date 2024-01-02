2c165df9d7b2572ad8c71693c953ff4c1bcb7432
ivymso13 <37726188+ivymso13@users.noreply.github.com>
2024-01-02 16:04:09 +0900

    [Gold II] Title: 개미의 이동, Time: 168 ms, Memory: 2688 KB -BaekjoonHub

---
+++ 백준/Gold/6612. 개미의 이동/개미의 이동.cc
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <utility>
+#include <algorithm>
+#define NMAX 100010
+using namespace std;
+typedef pair<int, int> PAIR;
+
+int L, A, p;
+char d;
+
+int cntL, l, r;
+PAIR inp[NMAX], ret[NMAX];
+
+bool cmp(PAIR a, PAIR b) { return abs(a.first) < abs(b.first); }
+
+int main() {
+    while(scanf("%d %d\n", &L, &A) != EOF) {
+        // 초깃값
+        cntL = 0;
+        l = r = 0;
+
+        // 입력
+        for(int i=0;i<A;i++) {
+            scanf("%d %c\n", &p, &d);
+
+            if(d == 'R') inp[i] = {p, 1};
+            else {
+                inp[i] = {p, -1};
+                cntL++;
+            }
+        }
+
+        // 절댓값 기준으로 정렬
+        sort(inp, inp+A, cmp);
+
+        for(int i=0;i<A;i++) {
+            // 오른쪽 방향 개미만큼 왼쪽으로 떨어짐
+            if(i<cntL) {
+                // 가장 가까운 왼쪽 방향 개미 찾기 >> 바톤터치
+                while(inp[l].second > 0) l++;
+
+                ret[i] = {inp[l++].first, inp[i].first};
+            }
+                // 나머지는 오른쪽으로 떨어짐
+            else {
+                // 가장 가까운 오른쪽 방향 개미 찾기 >> 바톤터치
+                while(inp[r].second < 0) r++;
+
+                ret[i] = {L - inp[r++].first, inp[i].first};
+            }
+        }
+
+        // 결괏값 정렬
+        sort(ret, ret+A);
+
+        // 출력
+        // 2개 이상인 경우에는 모두 출력하기
+        if(A>=2 and ret[A-1].first == ret[A-2].first)
+            printf("The last ant will fall down in %d seconds - started at %d and %d.\n", ret[A-1].first, ret[A-2].second, ret[A-1].second);
+        else
+            printf("The last ant will fall down in %d seconds - started at %d.\n", ret[A-1].first, ret[A-1].second);
+    }
+}
