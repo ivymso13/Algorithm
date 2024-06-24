65b5b2950c1d0865abf654f19167f64d3a083afb
ivymso13 <37726188+ivymso13@users.noreply.github.com>
2024-06-24 09:54:31 +0900

    [Silver V] Title: Diamond Collector (Bronze), Time: 0 ms, Memory: 1188 KB -BaekjoonHub

--- 백준/Silver/12005. Diamond Collector （Bronze）/Diamond Collector （Bronze）.cc
+++ 백준/Silver/12005. Diamond Collector （Bronze）/Diamond Collector （Bronze）.cc
@@ -1,35 +1,30 @@
 #include <stdio.h>
 #include <vector>
-#include <map>
 #include <algorithm>
 using namespace std;
 
 int N, K, t;
 vector< int > inp;
 
-int l, r, len;
+int l, r;
 int tmp, ret;
-map<int, int> check;
 
 int main() {
     scanf("%d %d", &N, &K);
     for(int i=1;i<=N;i++) {
         scanf("%d", &t);
-        
-        check[t]++;
         inp.push_back(t);
     }
     
     sort(inp.begin(), inp.end());
-    inp.erase(unique(inp.begin(), inp.end()), inp.end());
-    len = (int)inp.size();
     
     l = r = 0;
-    while(r < len) {
-        if(inp[r] - inp[l] > K) tmp -= check[inp[l++]];
-        else tmp += check[inp[r++]];
-        
-        ret = max(ret, tmp);
+    while(r < N) {
+        if(inp[r] - inp[l] > K) l++;
+        else {
+            ret = max(ret, r-l+1);
+            r++;
+        }
     }
     
     printf("%d", ret);
